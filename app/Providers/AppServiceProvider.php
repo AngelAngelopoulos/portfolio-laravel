4179215999609b26f1d7e8f02e5fc5e1edb86262
AngelAngelopoulos <angelsuminoe01@gmail.com>
2020-09-19 04:58:03 +0000

    prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') == 'production')
         {
             URL::forceScheme('https');
+	\Debugbar::disable();
         }
 
         //parent::boot();
